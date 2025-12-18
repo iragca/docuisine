4e3785631c758c490db6dffc6aded1e37cdf64dc
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-18 11:59:10 +0800

    style: format Password annotation for better readability

--- docuisine/schemas/annotations.py
+++ docuisine/schemas/annotations.py
@@ -9,7 +9,9 @@ from docuisine.db.database import SessionLocal
 from docuisine.utils.validation import validate_password, validate_version
 
 CommitHash = Annotated[str, Len(7, 7)]
-Password = Annotated[str, Len(min_length=8, max_length=128), AfterValidator(validate_password)]
+Password = Annotated[
+    str, Len(min_length=8, max_length=128), AfterValidator(validate_password)
+]  # Unhashed password
 Version = Annotated[str, MinLen(5), AfterValidator(validate_version)]
 
 
