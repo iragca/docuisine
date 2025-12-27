d1df0b9add505a378d10f31e008cae7fabcec484
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-27 21:15:07 +0800

    fix: update 401 error message detail to match FastAPI's

--- docuisine/utils/errors/auth.py
+++ docuisine/utils/errors/auth.py
@@ -24,5 +24,5 @@ ForbiddenAccessError = HTTPException(
 
 UnauthorizedError = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Could not validate credentials.",
+    detail="Not authenticated",
 )
